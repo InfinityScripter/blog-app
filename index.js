dc117bde95661b4e07e51bf2e92f6643b313fee7
InfinityScripter <talalaev.misha@gmail.com>
2025-01-12 23:24:05 +0300

    (feat) подключили multer, сделали возможность сохранения файлов на локальной машине и переход по роутам

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {loginValidation, postCreateValidation, registerValidation} from "./valid
 import checkAuth from "./utils/check_auth.js"
 import * as userController from "./controllers/user_controller.js";
 import * as postController from "./controllers/post_controller.js";
+import multer from "multer";
 
 mongoose.connect(
     'mongodb+srv://Mikhail:Sa54CsaA6Sk1QDJL@cluster1.hwqs0am.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster1'
@@ -14,7 +15,23 @@ mongoose.connect(
     console.log('DB error', err)
 })
 
+// Создаем app через express и подключаем mongoose
 const app = express();
+// Создаем папку для загрузки файлов (хранилище) через multer
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+// Подключаем multer
+const upload = multer({storage})
+
+// Нужно express дать знать, что есть папка uploads в кторой хранится файлы
+app.use('/uploads', express.static('uploads'))
+// Подключаем express к нашему приложению
 app.use(express.json()); // for parsing application/json, подключаем что бы принимать json
 
 app.post('/auth/login', loginValidation, userController.login)
@@ -30,6 +47,13 @@ app.get('/posts/:id', postController.getOne)
 app.delete('/posts/:id', checkAuth, postController.remove)
 app.patch('/posts/:id', checkAuth, postController.update)
 
+// Выполняем сохраниение файлов через multer в папку uploads .
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
 app.listen(4444, (err) => {
     if (err) throw err
     console.log('Example OK, app listening on port 4444!')
