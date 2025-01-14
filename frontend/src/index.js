47e5b24848fc08e2ca819c57b5c7d32568868fa1
InfinityScripter <talalaev.misha@gmail.com>
2025-01-14 12:56:18 +0300

    создаем стор и слайс RTK для постов с тегами

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {BrowserRouter} from "react-router-dom";
 
 import "./index.scss";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+import {ThemeProvider} from "@mui/material";
+import {theme} from "./theme";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-      <App />
-        </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <CssBaseline/>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <App/>
+                </Provider>
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
 );
