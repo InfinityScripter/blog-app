d2986a757ff9474a451ad8c63bca728c9a2dcb14
InfinityScripter <talalaev.misha@gmail.com>
2025-01-15 15:07:39 +0300

    1. в axios.js добавили в headers токен к запросам, если пользователь авторизован

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -5,4 +5,11 @@ const instance = axios.create({
     baseURL: 'http://localhost:4444'
 })
 
+// добавляем заголовок с токеном в каждый запрос если пользователь авторизован,
+// что бы проверить его доступы
+instance.interceptors.request.use(config => {
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+})
+
 export default instance
