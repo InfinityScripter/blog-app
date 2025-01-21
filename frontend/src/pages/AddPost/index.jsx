f8b2d9ac403a219b66a9af56d5473b1c04356097
InfinityScripter <talalaev.misha@gmail.com>
2025-01-21 14:54:57 +0300

    (feat)
    Добавил функционал обновления статей

--- frontend/src/pages/AddPost/index.jsx
+++ frontend/src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,12 +8,13 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import {useSelector} from "react-redux";
 import {selectIsAuth} from "../../redux/slices/auth";
-import {Navigate, useNavigate} from "react-router-dom";
+import {Navigate, useNavigate, useParams} from "react-router-dom";
 import axios from "../../axios";
 import {Card} from "@mui/material";
 import Cookies from "js-cookie";
 
 export const AddPost = () => {
+    const {id} = useParams()
     const navigate = useNavigate();
     const isAuth = useSelector(selectIsAuth)
     const [imageUrl, setImageUrl] = React.useState('');
@@ -22,6 +23,8 @@ export const AddPost = () => {
     const [value, setValue] = React.useState('');
     const inputFileRef = React.useRef(null);
     const [loading, setLoading] = React.useState(false);
+
+    const isEditing = Boolean(id);
 
     const handleChangeFile = async (e) => {
         try {
@@ -54,15 +57,33 @@ export const AddPost = () => {
                 tags: tags.split(',').map(tag => tag.trim()),
                 text: value,
             }
-            const {data} =  await axios.post('/posts', fields);
-            const id = data.post._id;
-            navigate(`/posts/${id}`)
+            // Если создание статьи, то post иначе update
+            const {data} =  isEditing ?
+                await axios.patch(`/posts/${id}`, fields) :
+                await axios.post('/posts', fields);
+            const postId = isEditing ? id : data.post._id;
+            navigate(`/posts/${postId}`);
             console.log(data)
         }
         catch (err){
             console.log(err)
         }
     };
+
+    useEffect(() => {
+        // Если id есть, то загружаем пост и заполняем поля существующими данными
+        if (id) {
+            axios.get(`/posts/${id}`).then(({data}) => {
+                setTitle(data.title);
+                setValue(data.text);
+                setImageUrl(data.imageUrl);
+                setTags(data.tags.join(','));
+            })
+        }
+
+
+    }, []);
+
 
     const options = React.useMemo(
         () => ({
@@ -121,7 +142,7 @@ export const AddPost = () => {
             <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options}/>
             <div className={styles.buttons}>
                 <Button onClick={onSubmit} size="large" variant="contained">
-                    Опубликовать
+                    {isEditing ? 'Сохранить' : 'Опубликовать'}
                 </Button>
                 <a href="/">
                     <Button size="large">Отмена</Button>
