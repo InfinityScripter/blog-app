71caf2ba4fa4c10eec31dc6eb1307fbe08394c15
InfinityScripter <talalaev.misha@gmail.com>
2025-01-29 14:01:42 +0300

    add new theme in project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,22 +5,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {BrowserRouter} from "react-router-dom";
 
 import "./index.scss";
-import {ThemeProvider} from "@mui/material";
-import {theme} from "./theme";
+
 import {Provider} from "react-redux";
 import store from "./redux/store";
+import AppTheme from "./theme/AppTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
         <CssBaseline/>
-        <ThemeProvider theme={theme}>
+        <AppTheme>
             <BrowserRouter>
                 <Provider store={store}>
                     <App/>
                 </Provider>
             </BrowserRouter>
-        </ThemeProvider>
+        </AppTheme>
     </>
 );
