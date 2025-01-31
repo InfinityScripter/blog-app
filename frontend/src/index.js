98f4953ea07232a27d9106b75d7c246a1a4f349b
InfinityScripter <talalaev.misha@gmail.com>
2025-01-31 18:28:03 +0300

    (style) fix  прокрутка страницы без проскальзывания и эффекта желе

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,27 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import {BrowserRouter} from "react-router-dom";
-import { StyledEngineProvider } from '@mui/material/styles';
+import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import "./index.scss";
 
-
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/store";
 import AppTheme from "./theme/AppTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <>
-        <StyledEngineProvider injectFirst>
-        <CssBaseline enableColorScheme/>
-        <AppTheme>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </AppTheme>
-        </StyledEngineProvider>
-    </>
+  <>
+    <StyledEngineProvider injectFirst>
+      <CssBaseline enableColorScheme />
+      <AppTheme>
+        <BrowserRouter>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </BrowserRouter>
+      </AppTheme>
+    </StyledEngineProvider>
+  </>
 );
